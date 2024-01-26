f9ac15d8993ddb1c9ae0def4721a184040032c77
Oliver <oliver.a.jarjosa@gmail.com>
2024-01-25 19:02:50 -0500

    push

---
+++ app.js
@@ -0,0 +1,61 @@
+//Variables
+var catOrange = 10;
+var catYellow = 20;
+var catBlue = 30;
+var catGreen = 40;
+var catPurple = 50;
+var dogOrange = 60;
+var dogYellow = 70;
+var dogBlue = 80;
+var dogGreen = 90;
+var dogPurple = 100;
+
+//Addition
+
+function addTwo(catOrange, catBlue,) {
+    console.log(catOrange + catBlue);
+}
+
+function addTwo(catOrange, catYellow,) {
+    console.log(catOrange + catYellow);
+}
+
+//Subtraction
+
+function minusTwo(dogPurple, dogGreen,) {
+    console.log(dogPurple - dogGreen);
+}
+
+function minusTwo(dogGreen, catBlue,) {
+    console.log(dogGreen - catBlue);
+}
+
+//Multiplication
+
+function timesTwo(catPurple, dogOrange,) {
+    console.log(catPurple * dogOrange);
+}
+
+function timesTwo(catPurple, dogPurple,) {
+    console.log(catPurple * dogPurple);
+}
+
+//Division
+
+function divTwo(dogPurple, catYellow,) {
+    console.log(dogPurple / catYellow);
+}
+
+function divTwo(dogYellow, catYellow,) {
+    console.log(dogYellow / catYellow);
+}
+
+//Decrement
+let catOrange = 10;
+catOrange--;
+//catOrange is now 9
+
+//Increment
+let dogOrange = 60;
+dogOrange++;
+//dogOrange is now 61
